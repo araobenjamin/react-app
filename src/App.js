155b036fa8d054eed82c1f881e8dc5e14125209f
Arão Benjamin Garcéa <arao.benjamin@gmail.com>
2024-07-20 22:33:22 -0300

    init

--- src/App.js
+++ src/App.js
@@ -17,9 +17,62 @@ function App() {
         >
           Learn React
         </a>
+        <MyButton />
       </header>
+      <Profile />
+      <ProductList />
     </div>
   );
 }
 
 export default App;
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked the button!');
+  }
+  return (
+    <button onClick={handleClick}>Click me!</button>
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+function ProductList() {
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+
+  const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.title}
+    </li>
+  )
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
